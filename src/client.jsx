d4124cd04598bbf5adca5056d0d02c3c3184483e
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2018-11-07 17:45:16 +0100

    Added a simple ReactJS framework for handling changing pages.

---
+++ src/client.jsx
@@ -0,0 +1,76 @@
+import { h, render } from 'preact'
+
+const header = (links) => {
+	return (
+		<header>
+			<h1>Code Moth</h1>
+			<h2>A weekly hangout for learning about computers</h2>
+			<nav>
+				{links.map(link => {
+					return <button onClick={link.action}>{link.text}</button>
+				})}
+			</nav>
+		</header>
+	)
+}
+
+const home = () => {
+	return (
+		<main>
+		  <div class="row">
+					<img src="Room.png"/>
+					<div class="blurb">
+						Code Moth is an <b>open weekly hangout</b> for everyone who wants to learn and work with IT for a better world.
+					</div>
+					<div class="blurb">
+						<h2>Be motivated</h2>by meeting others with similar interests. Having people around you who are interested in what you do helps you progress, and you can either work silently or ask others for feedback.
+					</div>
+					<div class="blurb">
+						<h2>Ask for help</h2> when you're stuck. There are experienced IT-people at the hangout who will gladly help you out!
+					</div>
+			</div>
+        
+			<div class="row">
+				<img src="Stairs.png"/>
+					<div class="text">
+						2nd floor<br/>
+						Linnégatan 21<br/>
+						Thursdays 18:00 - 20:00
+					</div>
+			</div>
+       
+			<div class="row">
+				<div class="text">
+					<span id="outer-door-locked">Contact <span id="none-phumber">[PHONE NUMBER]</span> to enter the building, in case the outer door is locked. You can also send a text message if you have questions about the hangout.</span>
+				</div>
+			</div>
+		</main>
+	)
+}
+
+const otherPage = () => {
+	return <h1>hi lol</h1>
+}
+
+function app(header, page) {
+	/*
+	const links = [
+		{
+			action: () => app(header, home),
+			text: "Home"
+		},
+		{
+			action: () => app(header, otherPage),
+			text: "Other"
+		}
+	]
+ */
+	const links = []
+	render(<div id="root">
+				 {header(links)}
+				 {page()}
+				 </div>,
+				document.body, document.getElementById("root"))
+}
+
+app(header, home)
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+var APP_DIR = path.resolve(__dirname, './src/client.jsx')
+
+module.exports = {
+  watch: true,
+  entry: APP_DIR,
+  output: {
+    filename: 'client.js',
+    path: path.resolve(__dirname, '.')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'node_modules')
+    ]
+  }
+}
