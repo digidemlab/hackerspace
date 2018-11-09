9142f9fef4035454cbad76d9a34f400746be09f6
Sebastian <sebastian.vasser@gmail.com>
2018-11-09 20:18:18 +0100

    Created a proper routing system, and added info about an upcoming lecture.

--- src/client.jsx
+++ src/client.jsx
@@ -1,76 +1,112 @@
 import { h, render } from 'preact'
+import Router from 'preact-router'
+import createHashHistory from 'history/createHashHistory'
 
-const header = (links) => {
+const Header = (props) => {
 	return (
 		<header>
 			<h1>Code Moth</h1>
 			<h2>A weekly hangout in Gothenburg for learning about computers</h2>
+
 			<nav>
-				{links.map(link => {
-					return <button onClick={link.action}>{link.text}</button>
+				{props.links.map(link => {
+					return <a className={
+            window.location.hash == link.href ? 'active' : ''
+          } href={link.href}>{link.text}</a>
 				})}
 			</nav>
 		</header>
 	)
 }
 
-const home = () => {
+const Home = (props) => {
 	return (
 		<main>
 		  <div class="row">
-					<img src="Room.png"/>
-					<div class="blurb">
-						Code Moth is an <b>open weekly hangout</b> in Gothenburg, Sweden for everyone who wants to learn and work with IT for a better world.
-					</div>
-					<div class="blurb">
-						<h2>Be motivated</h2>by meeting others with similar interests. Having people around you who are interested in what you do helps you progress, and you can either work silently or ask others for feedback.
-					</div>
-					<div class="blurb">
-						<h2>Ask for help</h2> when you're stuck. There are experienced IT-people at the hangout who will gladly help you out!
-					</div>
+        <img src="Room.png"/>
+        <p>
+          Code Moth is an <em>open weekly hangout</em> in Gothenburg, Sweden for everyone who wants to learn and work with IT for a better world.
+        </p>
+        <p>
+          <h2>Be motivated</h2>by meeting others with similar interests. Having people around you who are interested in what you do helps you progress, and you can either work silently or ask others for feedback.
+        </p>
+        <p>
+          <h2>Ask for help</h2> when you're stuck. There are experienced IT-people at the hangout who will gladly help you out!
+        </p>
 			</div>
         
 			<div class="row">
 				<img src="Stairs.png"/>
-					<div class="text">
-						2nd floor<br/>
-						Linnégatan 21, Göteborg<br/>
-						Thursdays 18:00 - 20:00
-					</div>
+        <p class="thin">
+          2nd floor<br/>
+          Linnégatan 21, Göteborg<br/>
+          Thursdays 18:00 - 20:00
+        </p>
 			</div>
        
 			<div class="row">
-				<div class="text">
-					<span id="outer-door-locked">Contact <span id="none-phumber">[PHONE NUMBER]</span> to enter the building, in case the outer door is locked. You can also send a text message if you have questions about the hangout.</span>
-				</div>
+				<p class="thin">
+					Contact <span id="none-phumber">070 754 70 12</span> to enter the building, in case the outer door is locked. You can also send a text message if you have questions about the hangout.
+				</p>
 			</div>
 		</main>
 	)
 }
 
-const otherPage = () => {
-	return <h1>hi lol</h1>
+const Lectures = () => {
+	return (
+    <main>
+      <div class="pageblurb">
+        <h1>Lectures</h1>
+        <p>Sometimes we hold a public lecture or workshop at Code Moth. Here are the latest lectures you can participate in.</p>
+      </div>
+      <div class="row">
+        <h1>Why should I learn programming?</h1>
+        <h2 class="tagline">A lecture on the applications of programming</h2>
+        <img src="focused_radu_and_seb.png"/>
+        <p>
+          Thursday, 29th of November<br/>
+          18:00-19:00<br/>
+          Language: English if needed, otherwise Swedish.<br/>
+          Required knowledge: None, everyone is welcome.<br/>
+          Place: Linnégatan 21, Göteborg (our usual location)
+        </p>
+        <p>
+          Programming is a popular subject nowadays, and many want to learn more about it. But what is it, really? In this lecture we explore a couple of examples that show how widespread programming really is, and what possibilities appear after one learns a programming language.
+        </p>
+        <p>
+          After the lecture there will be an open hangout where everyone is welcome to hang out and talk, code, ask questions and learn things about computers. Everyone is welcome no matter previous knowledge.
+        </p>
+        <p>
+          Depending on how many are interested we might put up a signup form, otherwise you can just join without signing up.
+        </p>
+      </div>
+    </main>
+  )
 }
 
-function app(header, page) {
-	/*
+function App(props) {
 	const links = [
 		{
-			action: () => app(header, home),
+      href: "#/",
 			text: "Home"
 		},
 		{
-			action: () => app(header, otherPage),
-			text: "Other"
+      href: "#/lectures",
+			text: "Lectures"
 		}
 	]
- */
-	const links = []
-	render(<div id="root">
-				 {header(links)}
-				 {page()}
-				 </div>,
-				document.body, document.getElementById("root"))
+	return(
+    <div id="root">
+      <Header links={links}/>
+      <Router history={createHashHistory()}>
+        <Home path="/"/>
+        <Lectures path="/lectures"/>
+      </Router>
+    </div>
+	)
 }
 
-app(header, home)
+render(<App/>, document.body)
+
+window.onhashchange = () => render(<App/>, document.body, document.getElementById('root'))
