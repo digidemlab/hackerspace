439d5999eb075fa681694e2ed48b8740b533cbb8
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2019-04-06 12:00:31 +0200

    Updated location

---
+++ src/home.js
@@ -0,0 +1,42 @@
+import { render, h } from 'preact'
+
+export default props => {
+  document.title = "Code Moth"
+	return (
+		<main>
+		  <div class="row">
+        <img src="Room.png"/>
+        <p>
+          Code Moth is an <em>open weekly hangout</em> in Gothenburg, Sweden for everyone who wants to learn and work with IT for a better world.
+        </p>
+        <p>
+          <h2>Be motivated</h2>by meeting others with similar interests. Having people around you who are interested in what you do helps you progress, and you can either work silently or ask others for feedback.
+        </p>
+        <p>
+          <h2>Ask for help</h2> when you're stuck. There are experienced IT-people at the hangout who will gladly help you out!
+        </p>
+			</div>
+        
+			<div class="row">
+				<img src="Stairs.png"/>
+ *        <p class="thin">
+          2nd floor<br/>
+          Viktoriahuset, at Linnégatan 21, Göteborg<br/>
+          Tuesdays 18:00 - 20:00
+        </p>
+			</div>
+		</main>
+	)
+}
+
+/*
+ *        <p class="thin">
+          2nd floor<br/>
+          Normally we're at Linnégatan 21, Göteborg, but today (10th of January) we're at <a href="https://kalendarium.goteborg.se/wps/portal/enhetssida/motesplats-backa-brunnsbo-/kontakt" target="_blank">Mötesplats Backa-Brunnsbo</a>, Folkvisegatan 14.<br/>
+          Thursdays 18:00 - 20:00
+        </p>
+
+
+
+					Contact <span id="none-phumber">070 754 70 12</span> to enter the building, in case the outer door is locked. You can also send a text message if you have questions about the hangout.
+*/
